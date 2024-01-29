7be3a4a4df1429badbdef76aa6586c3e023557c7
Vanshiljain <115866666+Vanshiljain@users.noreply.github.com>
2024-01-29 15:02:31 +0530

    Update auth.guard.ts

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -5,7 +5,7 @@ import { GqlExecutionContext } from '@nestjs/graphql';
 @Injectable()    
 export class JwtAuthGuard extends AuthGuard('jwt') {    
   getRequest(context: ExecutionContext) {         
-    const ctx = GqlExecutionContext.create(context);     
+    const ctx = GqlExecutionContext.create(context);      
     const { req } = ctx.getContext();     
     const token = req.headers.authorization?.replace ('Bearer ', '');   
     req.token = token;
